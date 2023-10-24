6130d9f972f4ac7d8c01c4021716f24ef8d18758
ezzange <ezzang22@naver.com>
2023-10-25 00:19:04 +0900

    java

--- src/classss/ex5/MethodTest.java
+++ src/classss/ex5/MethodTest.java
@@ -13,7 +13,21 @@ public class MethodTest {
         System.out.println("x * y = " + myMath.multiply(x,y));
         System.out.println("x / y = " + myMath.divade(x,y));
         System.out.println("x 와 y 중 최댓값은? = " + myMath.max(x,y));
+        System.out.println("x 와 y 중 최댓값은? = " + myMath.max2(x,y));
         System.out.println("x 와 y 중 최솟값은? = " + myMath.min(x,y));
         System.out.println("x 와 y 의 평균값은? = " + myMath.average(x,y));
+
+
+        for (int i = 1; i <= 9; i++) {
+            myMath.printGuGudan(i);
+        }
+
+        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+        for (int i = 1; i <= 3; i++) {
+            int dan = (int) (Math.random() * 15);
+            System.out.println("dan = " + dan);
+            myMath.printGuGudan(dan);
+
+        }
     }
 }
--- src/classss/ex5/MyMath.java
+++ src/classss/ex5/MyMath.java
@@ -4,9 +4,11 @@ public class MyMath {
 
     public int add(int x, int y) {
         return x + y;
-    }public int subtract(int x, int y) {
+    }
+    public int subtract(int x, int y) {
         return x - y;
-    }public int multiply(int x, int y) {
+    }
+    public int multiply(int x, int y) {
         return x * y;
     }
     public int divade(int x, int y) {
@@ -18,6 +20,13 @@ public class MyMath {
     public int max(int x, int y) {
         return x > y? x : y; //Math.max(x, y)
     }
+
+    public int max2(int x, int y) {
+        if (x>y){
+            return x;
+        }else//if문은 조건문이 참일때만 실행되니 else로 거짓일 경우에 실행 되도록 추가
+            return y;
+    }
     public int add3000(int x) {
         return x + 3000;
     }
@@ -26,4 +35,14 @@ public class MyMath {
         return (x + y) / 2;
     }
 
+    public void printGuGudan(int dan) {
+        //1~9단이 아닌 경우 메서드는 실행되지 않고 종료
+        if (! (1<=dan && dan <=9)) {
+            return;
+        }
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d%n", dan, i, dan * i);
+                    //형식(int, float, double 등)에 구애 받지 않는 문자를 그대로 출력
+        }
+    }
 }
