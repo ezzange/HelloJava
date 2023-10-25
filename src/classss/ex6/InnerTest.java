6c08ce15c0596f288ac166ffa26498732e088fff
ezzange <ezzang22@naver.com>
2023-10-26 03:21:14 +0900

    java

--- src/classss/ex6/InnerTest.java
+++ src/classss/ex6/InnerTest.java
@@ -1,16 +1,12 @@
 package classss.ex6;
 
-public class InnerTest {
-    public static void main(String[] args) {
-        System.out.println(InstanceInner.CONST);
-        System.out.println(StaticInner.cv);
-//        System.out.println(StaticInner.iv); ERROR
-//        System.out.println(LocalInner.CONST); ERROR 지역 변수 내부클래스는 메서드내에서만.
+class InnerTest {
+    private int aaa = 0;
 
-    }
     class InstanceInner{ // 인스턴스 내부 클래스
         int iv = 100;
-        static int cv = 100; // ERROR static 내부 클래스만 static 멤버 정의 가능
+        int iiv = aaa; // 내부클래스에서 외부 클래스의 private변수에 접근 가능.
+        static int cv = 100; // ERROR static 내부 클래스만 static 멤버 정의 가능????
         final static int CONST = 100; // final static 는 상수라 가능
     }
 
@@ -22,9 +18,19 @@ public class InnerTest {
     void method(){
         // local 메소드 내부 클래스는 해당 속해있는 외부 클래스의 멤버 모두 접근 가능
         class LocalInner{ // 지역 내부 클래스
-            int iv = 300;
-            static int cv = 300; // ERROR static 내부 클래스만 static 멤버 정의 가능
+            int lv = 300;
+            static int cv = 300; // ERROR static 내부 클래스만 static 멤버 정의 가능?????
             final static int CONST = 300; // final static 는 상수라 가능
         }
+        System.out.println(LocalInner.cv);
+    }
+    public static void main(String[] args) {
+        System.out.println(InstanceInner.CONST);
+        System.out.println(InstanceInner.cv);
+        System.out.println(StaticInner.cv);
+//        System.out.println(StaticInner.iv); ERROR
+//        System.out.println(LocalInner.CONST); ERROR 지역 변수 내부클래스는 메서드내에서만.
+
     }
 }
+
