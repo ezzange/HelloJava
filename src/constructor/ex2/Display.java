d6515fe2b3e7d9d5c01b16dd01b40c66701d3c96
ezzange <ezzang22@naver.com>
2023-10-26 21:18:47 +0900

    java

--- src/constructor/ex2/Display.java
+++ src/constructor/ex2/Display.java
@@ -9,9 +9,10 @@ public class Display {
     }
 
     Display(String color, String panel, int size) { //매개변수를 가지고 있는 생성자
-        this.color = color;
-        this.panel = panel;
-        this.size =size;
+       // 자기 자신을 가르키고 있는 this .
+        this.color = color; // 들어오는 매개변숫값이 자기 자신의 변숫값에 대입
+        this.panel = panel; // => Display 의 iv panel 과 매개변수로 들어오는 panel과 값이 같도록 설정
+        this.size =size; //매개변수와 인스턴스 변수 이름이 같을 때는 구분을 위해 this 사용.
     }
 
 }
--- src/inheritance/ex5/Child2.java
+++ src/inheritance/ex5/Child2.java
@@ -4,8 +4,8 @@ class Child2 extends Parent2{
     int x = 20;
 
     public void method() {
-        System.out.println("x = "+ x);
-        System.out.println("this.x = "+ this.x);
-        System.out.println("super.x = "+ super.x);
+        System.out.println("x = "+ x); // Child2의 x
+        System.out.println("this.x = "+ this.x); // Child2의 x
+        System.out.println("super.x = "+ super.x); // Parent2의 x
     }
 }
