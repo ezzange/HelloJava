0c03e7b7d5734e26aa58c71dc2dfe32fe7e64f23
ezzange <ezzang22@naver.com>
2023-10-29 03:18:06 +0900

    java

--- src/Generics/ex3/GenericTest3.java
+++ src/Generics/ex3/GenericTest3.java
@@ -1,7 +1,19 @@
 package Generics.ex3;
 
+import interfacee.ex8.C;
+
 public class GenericTest3 {
     public static void main(String[] args) {
 
+        FruitBox<Fruit> fruitBox = new FruitBox<Fruit>();
+        FruitBox<Apple> appleBox = new FruitBox<Apple>();
+        FruitBox<PineApple> pineAppleBox = new FruitBox<PineApple>();
+        FruitBox<Cherry> CherryBox = new FruitBox<Cherry>();
+//        FruitBox<Toy> ToyBox = new FruitBox<Toy>(); //컴파일 에러 Fruit를 상속받지 않아 컴파일 에러 발생
+
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Fruit());
+        fruitBox.add(new Fruit());
     }
 }
